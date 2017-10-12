fcb1b02c4a55c91a6fccf810aa0bb0830bab543f
Miles Macchiaroli <macchiarolim@wit.edu>
2017-10-12 13:56:56 -0400

    Added comments, code, tested

--- src/edu/wit/cs/comp1000/LA1a.java
+++ src/edu/wit/cs/comp1000/LA1a.java
@@ -1,9 +1,36 @@
 package edu.wit.cs.comp1000;
-
+import java.util.Scanner;
+/**
+ * LA1a
+ * @author Miles Macchiaroli
+ *
+ */
 public class LA1a {
-
+	/**
+	 * This Program Calculates sum, mean, and standard deviation
+	 * for five numbers
+	 * @param args
+	 */
 	public static void main(String[] args) {
-		// TODO: write your code here
+		Scanner input = new Scanner(System.in);
+		//INT Vars
+		double in0, in1, in2, in3, in4, sum, mean, dev;
+		// Begin
+		System.out.printf("Enter five numbers: ");
+		in0 = input.nextDouble();
+		in1 = input.nextDouble();
+		in2 = input.nextDouble();
+		in3 = input.nextDouble();
+		in4 = input.nextDouble();
+		// Calculate
+		sum = (in0 + in1 + in2 + in3 + in4);
+		mean = (sum / 5);
+		dev = Math.sqrt(((Math.pow((in0 - mean), 2)) + (Math.pow((in1 - mean), 2)) + (Math.pow((in2 - mean), 2)) + (Math.pow((in3 - mean), 2)) + (Math.pow((in4 - mean), 2)))/(5));
+		//Print
+		System.out.printf("Sum: %.2f%n", sum);
+		System.out.printf("Mean: %.2f%n", mean);
+		System.out.printf("Population Standard Deviation: %.2f%n", dev);
+		//End
 	}
 
 }
